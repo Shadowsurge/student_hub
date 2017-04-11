1ed01d069ff67621d9cb3b9b32e24953297511c4
NazCassidy <nathan_cassidy@live.co.uk>
2017-04-11 15:32:09 +0100

    fixes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ let express = require('express'),
     passport = require('passport');
     passportLocalMongoose = require('passport-local-mongoose'),
     LocalStrategy = require('passport-local'),
-    mongoose = require('mongoose'),
+    {mongoose} = require('./db/mongoose'),
     flash = require('connect-flash');
 
 // ************************************************
@@ -24,16 +24,6 @@ let User = require('./models/user.js'),
 // ************************************************
 let app = express();
 
-// tell mongoose to use the javascrpt promise
-mongoose.Promise = global.Promise;
-
-let database =
-{
-    localHost: 'mongodb://localhost:27017/student_hub',
-    mlab: 'mongodb://admin:Studenthub1234@ds157500.mlab.com:57500/student_hub'
-}
-
-mongoose.connect(process.env.PORT ? database.mlab : database.localHost);
 const port = process.env.PORT || 3000;
 
 // Tell the app to use express session to manage cookies/user data etc
