9174bba1340687cc6ea4c3b82437e50eb03b3554
NazCassidy <nathan_cassidy@live.co.uk>
2017-04-25 20:00:56 +0100

    user login

--- routes/android.js
+++ routes/android.js
@@ -15,9 +15,9 @@ router.post('/android/login', passport.authenticate("local",
 router.get('/android/loginsuccess', (request, response) =>
 {
   // response.send("Logged in");
-  User.findById(request.user._id).then((loggedUser) =>
+  User.find(_id: request.user._id).then((loggedUser) =>
   {
-    response.json(JSON.stringify(loggedUser));
+    response.send(JSON.stringify(loggedUser));
     // let res = JSON.stringify({'success':'logged in', loggedUser});
     // return response.send(res);
   });
